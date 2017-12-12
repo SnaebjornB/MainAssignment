2e5523fffa33619e4759e21fbfd04886ce7e7588
SnaebjornB <34096981+SnaebjornB@users.noreply.github.com>
2017-12-12 14:17:53 +0000

    pizza breyting

--- MainAssignment/include/Model/pizza.h
+++ MainAssignment/include/Model/pizza.h
@@ -5,19 +5,32 @@
 #include <string>
 #include <vector>
 #include "topping.h"
+#include <fstream>
 
 using namespace std;
 
 class Pizza
 {
     public:
+        Pizza();
         Pizza(string name, int price, int size);
         vector<Topping> toppings;
+        void set_name(string name);
+        void set_price(int price);
+        void set_size(int size);
+        string get_name();
+        int get_price();
+        int get_size();
+        void set_helper(bool status);
+        friend ostream& operator << (ostream& out, Pizza& pizza);
+        friend istream& operator >> (istream& in, Pizza& pizza);
 
     private:
         string name;
         int price;
         int size;
+        bool pizza_helper;
+        string topping_name;
 };
 
 #endif // PIZZA_H
--- MainAssignment/src/Model/pizza.cpp
+++ MainAssignment/src/Model/pizza.cpp
@@ -1,9 +1,66 @@
 #include "pizza.h"
 
+Pizza::Pizza(){
+
+}
+
 Pizza::Pizza(string name, int price, int size){
     this->name = name;
     this->price = price;
     this->size = size;
 }
 
+void Pizza::set_name(string name){
+    this->name = name;
+}
 
+void Pizza::set_price(int price){
+    this->price = price;
+}
+
+void Pizza::set_size(int size){
+    this->size = size;
+}
+
+string Pizza::get_name(){
+    return this->name;
+}
+
+int Pizza::get_price(){
+    return this->price;
+}
+
+int Pizza::get_size(){
+    return this->size;
+}
+
+void Pizza::set_helper(bool status){
+    this->pizza_helper = status;
+}
+
+ostream& operator << (ostream& out, Pizza& pizza){
+    if(pizza.pizza_helper){
+        out << pizza.size << "\" " << "Pizza with:" << endl;
+        for(unsigned int i = 0; i < pizza.toppings.size(); i++){
+            out << pizza.toppings[i].get_name() << endl;
+        }
+    }
+    else{
+        out << pizza.name << ", " << pizza.price << " " << pizza.size << " ";
+        for(unsigned int i = 0; i < pizza.toppings.size(); i++){
+            out << pizza.toppings[i].get_name() << " ";
+        out << endl;
+        }
+    }
+    return out;
+}
+
+istream& operator >> (istream& in, Pizza& pizza){
+    getline(in, pizza.name, ',') >> pizza.price >> pizza.size;
+    for (int i = 0; !'\n'; i++){
+        in >> pizza.topping_name;
+        pizza.toppings[i].set_name(pizza.topping_name);
+    }
+    in.ignore();
+    return in;
+}
