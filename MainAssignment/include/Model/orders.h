7d25ed1497b2f1e1e9f5f6f2e67559de00ca2df4
SnaebjornB <34096981+SnaebjornB@users.noreply.github.com>
2017-12-13 15:20:50 +0000

    orders breytingar

--- MainAssignment/include/Model/orders.h
+++ MainAssignment/include/Model/orders.h
@@ -15,6 +15,33 @@ class Orders
     public:
         Orders();
 
+        bool get_baking_status();
+        bool get_ready_status();
+        bool get_paid_status();
+        bool get_delivered_status();
+        bool get_home_delivery_status();
+        void set_baking_status(bool status);
+        void set_ready_status(bool status);
+        void set_paid_status(bool status);
+        void set_delivered_status(bool status);
+        void set_home_delivery_status(bool status);
+        string get_comment();
+        string get_address();
+        string get_name();
+        string get_phone_number();
+        void set_comment(string change);
+        void set_address(string change);
+        void set_name(string change);
+        void set_phone_number(string change);
+        int get_total_price();
+        void set_total_price(int price);
+        void calculate_total();
+        void set_helper(bool status);
+        string get_name_of_place();
+        void set_name_of_place(string change);
+        friend ostream& operator << (ostream& out, Orders& orders);
+        friend istream& operator >> (istream& in, Orders& orders);
+
         vector<Sides> sides_ordered;
         vector<Menu_Pizza> menuPizzas_ordered;
         vector<Pizza> otherPizzas_ordered;
@@ -25,10 +52,12 @@ class Orders
         bool paid;
         bool delivered;
         bool home_delivery;
+        bool orders_helper;
         string comment;
         string address;
         string name;
         string phone_number;
+        string name_of_place;
         int total_price;
 
 
--- MainAssignment/src/Model/orders.cpp
+++ MainAssignment/src/Model/orders.cpp
@@ -2,7 +2,170 @@
 
 Orders::Orders()
 {
-    //ctor
+    orders_helper = true;
+    baking = false;
+    ready = false;
+    paid= false;
+    delivered= false;
+    home_delivery= false;
+    comment[0] = '\0';
+    address[0] = '\0';
+    name[0] = '\0';
+    phone_number[0] = '\0';
+    total_price = 0;
 }
 
+bool Orders::get_baking_status(){
+    return this->baking;
+}
 
+bool Orders::get_ready_status(){
+    return this->ready;
+}
+
+bool Orders::get_paid_status(){
+    return this->paid;
+}
+
+bool Orders::get_delivered_status(){
+    return this->delivered;
+}
+
+bool Orders::get_home_delivery_status(){
+    return this->home_delivery;
+}
+
+void Orders::set_baking_status(bool status){
+    this->baking = status;
+}
+
+void Orders::set_ready_status(bool status){
+    this->ready = status;
+}
+
+void Orders::set_paid_status(bool status){
+    this->paid = status;
+}
+
+void Orders::set_delivered_status(bool status){
+    this->delivered = status;
+}
+
+void Orders::set_home_delivery_status(bool status){
+    this->home_delivery = status;
+}
+
+string Orders::get_comment(){
+    return this->comment;
+}
+
+string Orders::get_address(){
+    return this->address;
+}
+
+string Orders::get_name(){
+    return this->name;
+}
+
+string Orders::get_phone_number(){
+   return this->phone_number;
+}
+
+string Orders::get_name_of_place(){
+    return this->name_of_place;
+}
+
+void Orders::set_comment(string change){
+    this->comment = change;
+}
+
+void Orders::set_address(string change){
+    this->address = change;
+}
+
+void Orders::set_name(string change){
+    this->name = change;
+}
+
+void Orders::set_phone_number(string change){
+    this->phone_number = change;
+}
+
+void Orders::set_name_of_place(string change){
+    this->name_of_place = change;
+}
+
+int Orders::get_total_price(){
+    return this->total_price;
+}
+
+void Orders::set_total_price(int price){
+    this->total_price = price;
+}
+
+void Orders::calculate_total(){
+    for(unsigned int i = 0; i < sides_ordered.size(); i++){
+        total_price += sides_ordered[i].get_price();
+    }
+    for(unsigned int i = 0; i < menuPizzas_ordered.size(); i++){
+        total_price += menuPizzas_ordered[i].get_price();
+    }
+    for(unsigned int i = 0; i < otherPizzas_ordered.size(); i++){
+        total_price += otherPizzas_ordered[i].get_price();
+    }
+}
+
+void Orders::set_helper(bool status){
+    this->orders_helper = status;
+}
+
+ostream& operator << (ostream& out, Orders& orders){
+    if(orders.orders_helper){
+        out << "----------------------------------------------" << endl << endl
+            << "Sími: " << orders.phone_number
+            << "Nafn: " << orders.name << endl
+            << orders.name_of_place << endl
+            << orders.comment << endl;
+        for(unsigned int i = 0; i < orders.menuPizzas_ordered.size(); i++){
+            orders.menuPizzas_ordered[i].set_helper(true);
+            out << orders.menuPizzas_ordered[i] << endl;
+        }
+        for(unsigned int i = 0; i < orders.otherPizzas_ordered.size(); i++){
+            orders.otherPizzas_ordered[i].set_helper(true);
+            out << orders.otherPizzas_ordered[i] << endl;
+        }
+        for(unsigned int i = 0; i < orders.sides_ordered.size(); i++){
+            orders.otherPizzas_ordered[i].set_helper(true);
+            out << orders.sides_ordered[i] << endl;
+        }
+        out << '\t' << '\t' << "Total: " << orders.total_price << endl << endl
+            << "----------------------------------------------" << endl;
+
+    }
+    else{
+        out << orders.phone_number << "," << orders.name << "," << orders.address << ","
+            << orders.comment << " " << orders.baking << " " << orders.ready << " " << orders.paid
+            << " " << orders.delivered << " " << orders.home_delivery << " " << orders.orders_helper
+            << " " << orders.total_price;
+
+        for(unsigned int i = 0; i < orders.menuPizzas_ordered.size(); i++){
+            orders.menuPizzas_ordered[i].set_helper(false);
+            out << orders.menuPizzas_ordered[i];
+        }
+        for(unsigned int i = 0; i < orders.otherPizzas_ordered.size(); i++){
+            orders.otherPizzas_ordered[i].set_helper(false);
+            out << orders.otherPizzas_ordered[i];
+        }
+        for(unsigned int i = 0; i < orders.sides_ordered.size(); i++){
+            orders.otherPizzas_ordered[i].set_helper(false);
+            out << orders.sides_ordered[i];
+        }
+        out << endl;
+    }
+
+    return out;
+}
+
+istream& operator >> (istream& in, Orders& orders){
+    return in;
+}
